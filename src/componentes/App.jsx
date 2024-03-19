b0d8d47a661a4699edc04caa17f529ee74e47d30
rodrigo arguello <nahu.99arguello@gmail.com>
2024-03-19 19:04:27 -0300

    Primer commit

---
+++ src/componentes/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "./shared/Sidebar"
+
+function App() {
+ 
+
+  return (
+  
+    <div className="text-8xl">
+      <Sidebar />
+    </div>
+  
+  );
+}
+
+export default App
---
+++ src/componentes/shared/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Sidebar = () =>{
+    return (
+        <div className="bg-red-400">side bar</div>
+    )
+}
+
+
+export default Sidebar
